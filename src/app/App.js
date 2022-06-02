577e64029de21cec8785d795d3c6081920d65887
Sergey <maxim@Sergey.sergei>
2022-06-02 11:59:09 +0300

    statr

---
+++ src/app/App.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import {Route, Switch, Redirect} from 'react-router-dom'
+
+function App() {
+  return (
+    <>
+      {/*<NavBar/>*/}
+      {/*<ContainerWrapper>*/}
+      {/*  <Switch>*/}
+      {/*    <Route path="/users/:userId?" component={Users}/>*/}
+      {/*    <Route path="/useCallback" component={UseCallbackExample}/>*/}
+      {/*    <Route path="/memo" component={MemoExample}/>*/}
+      {/*    <Route path="/useMemo" component={UseMemoExample}/>*/}
+      {/*    <Route*/}
+      {/*      path="/cloneElement"*/}
+      {/*      component={CloneElementExample}*/}
+      {/*    />*/}
+      {/*    <Route path="/hoc" component={HOCLayout}/>*/}
+      {/*    <Route path="/children" component={ReactChildren}/>*/}
+
+      {/*    <Route path="/useRef" component={UseRefExample}/>*/}
+      {/*    <Route path="/" exact component={Main}/>*/}
+      {/*    <Redirect to="/"/>*/}
+      {/*  </Switch>*/}
+      {/*</ContainerWrapper>*/}
+    </>
+  )
+}
+
+export default App
